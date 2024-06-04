e22f6deefb39732d6c293e18b0497b8ad856d151
Tim Moonen <info@moonendesign.nl>
2024-06-04 23:29:25 +0200

    Created some logic to see who wins. I feel it can be made alot easyer, first try, here we go!

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-console.log("It works!")
-
 const MIN = 1
 const MAX = 4
+
+let humanScore
+let computerScore
+
+function youLose(humanChoice, computerChoice){
+    return "You choose ${humanChoice} and the compter choose ${computerChoice}. You lose!";
+}
+
 
 function computerChoice (MIN, MAX) {
     const minCeiled = Math.ceil(MIN);
@@ -17,6 +23,26 @@ function computerChoice (MIN, MAX) {
 
 function getHumanChoice() {
     const choice = prompt("Choose a hand: Rock, Paper or Scissors!");
-    const checkedChoice = choice.toLowerCase();
-    return checkedChoice
+    const humanChoice = choice.toLowerCase();
+    if (humanChoice == "rock" || checkedChoice == "paper" || checkedChoice == "scissors")
+        return humanChoice
+    else
+        return "Not a valid choice, try again!"
 }
+
+function playround(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice)
+        return "You both choosed the same, try again!"
+    else if (humanChoice == "rock" & computerChoice == "paper"){
+        return youLose(humanChoice, computerChoice)
+    } 
+    else if (humanChoice == "paper" & computerChoice == "scissors") {
+        return youLose(humanChoice, computerChoice)
+    }
+    else if (humanChoice == "paper" & computerChoice == "scissors") {
+        return youLose(humanChoice, computerChoice)
+    }
+    else
+        return "You choose ${humanChoice} and the compter choose ${computerChoice}. You win!"
+}
+
